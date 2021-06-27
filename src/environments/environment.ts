65676e7c55fb0869acad67f6ddb023fa243d7fad
Ezequiel Tavares <eot_ccb@hotmail.com>
2021-06-27 03:34:29 -0300

    Create environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebase: {
+    firebase: {
+      apiKey: "",
+      authDomain: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: "",
+      measurementId: ""
+    }
+  }
+};
